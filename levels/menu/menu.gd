81d3ea9d1fd76e842f83672bc7d9b82a538f27bc
David Lewis <mail@david-lewis.com>
2025-05-11 20:03:26 +1000

    Set up game with basic multiplayer support

---
+++ levels/menu/menu.gd
@@ -0,0 +1,52 @@
+extends Node
+
+@export var ui:Control
+@export var level_container:Node
+@export var level_scene:PackedScene
+@export var ip_line_edit:LineEdit
+@export var status_label:Label
+
+@export var not_connected_hbox:HBoxContainer
+@export var host_hbox:HBoxContainer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	multiplayer.connection_failed.connect(_on_connection_failure)
+	multiplayer.connected_to_server.connect(_on_connected_to_server)
+	
+
+func _on_host_button_pressed() -> void:
+	not_connected_hbox.hide()
+	host_hbox.show()
+	Lobby.create_game()
+	status_label.text = "Hosting"
+
+func _on_join_button_pressed() -> void:
+	not_connected_hbox.hide()
+	Lobby.join_game(ip_line_edit.text)
+	status_label.text = "Connecting..."
+
+
+func _on_play_button_pressed() -> void:
+	hide_menu.rpc()
+	change_level.call_deferred(level_scene)
+
+
+func change_level(scene:PackedScene):
+	for child in level_container.get_children():
+		level_container.remove_child(child)
+		child.queue_free()
+	level_container.add_child(scene.instantiate())
+
+func _on_connection_failure():
+	not_connected_hbox.show()
+	status_label.text = "Failed to connect"
+
+func _on_connected_to_server():
+	status_label.text = "Connected"
+
+# RPC will be called on connected clients and on the host, but 
+# can only be called by the host
+@rpc("call_local", "authority", "reliable")
+func hide_menu():
+	ui.hide()
---
+++ player/player.gd
@@ -0,0 +1,121 @@
+extends CharacterBody2D
+
+@export var player_sprite:AnimatedSprite2D
+@export var player_camera:PackedScene
+
+@export var camera_height := -132.0
+
+@export var movement_speed := 300
+@export var gravity := 30
+@export var jump_strength := 600
+@export var max_jumps := 3
+
+@onready var initial_sprite_scale := player_sprite.scale
+
+var owner_id := 1
+var jump_count := 0
+var camera_instance
+
+var state = PlayerStates.IDLE
+
+enum PlayerStates {
+	IDLE,
+	WALKING,
+	JUMP_STARTED,
+	JUMPING,
+	DOUBLE_JUMPING,
+	FALLING
+}
+
+func _enter_tree() -> void:
+	owner_id = name.to_int()
+	set_multiplayer_authority(owner_id)
+	
+	if owner_id != multiplayer.get_unique_id():
+		return
+		
+	set_up_camera()
+	
+func _process(delta: float) -> void:
+	if multiplayer.multiplayer_peer == null:
+		return
+
+	if owner_id != multiplayer.get_unique_id():
+		return
+	update_camera_pos()
+
+func _physics_process(_delta: float) -> void:
+	if owner_id != multiplayer.get_unique_id():
+		return	
+	var horizontal_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+
+	velocity.x = horizontal_input * movement_speed
+	velocity.y += gravity
+
+	handle_movement_state()
+		
+	move_and_slide()
+	
+	face_movement_direction(horizontal_input)
+
+func handle_movement_state():
+	# Decide State
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		state = PlayerStates.JUMP_STARTED
+	elif is_on_floor() and is_zero_approx(velocity.x):
+		state = PlayerStates.IDLE
+	elif not is_zero_approx(velocity.x) and is_on_floor():
+		state = PlayerStates.WALKING
+	else:
+		state = PlayerStates.JUMPING
+		
+	if velocity.y > 0.0 and not is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			state = PlayerStates.DOUBLE_JUMPING
+		else:
+			state = PlayerStates.FALLING
+		
+	# Processing state
+	match state:
+		PlayerStates.IDLE: 
+			player_sprite.play("idle")
+			jump_count = 0
+		PlayerStates.JUMP_STARTED:
+			player_sprite.play("jump_start")
+			jump_count += 1
+			velocity.y = -jump_strength
+		PlayerStates.DOUBLE_JUMPING:
+			player_sprite.play("double_jump_start")
+			jump_count += 1
+			if jump_count <= max_jumps:
+				velocity.y = -jump_strength
+		PlayerStates.WALKING:
+			player_sprite.play("walk")
+			jump_count = 0
+		PlayerStates.FALLING:
+			player_sprite.play("fall")
+
+	# Jump cancelling
+	if Input.is_action_just_released("jump") and velocity.y < 0.0: 
+		velocity.y = 0.0
+
+func face_movement_direction(horizontal_input):
+	if not is_zero_approx(horizontal_input):
+		if horizontal_input < 0:
+			player_sprite.scale = Vector2(-initial_sprite_scale.x, initial_sprite_scale.y)
+		else:
+			player_sprite.scale = initial_sprite_scale	
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if state == PlayerStates.JUMPING:
+		player_sprite.play("jump")
+
+func set_up_camera():
+	camera_instance = player_camera.instantiate()
+	camera_instance.global_position.y = camera_height
+	get_tree().current_scene.add_child.call_deferred(camera_instance)
+
+
+func update_camera_pos():
+	camera_instance.global_position.x = global_position.x
