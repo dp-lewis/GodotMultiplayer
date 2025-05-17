a6a26d2ca8ea81ed6d4b75af305ac75c7fb4a0b9
David Lewis <mail@david-lewis.com>
2025-05-18 09:37:02 +1000

    Add steam functionality

--- levels/menu/menu.gd
+++ levels/menu/menu.gd
@@ -10,6 +10,10 @@ extends Node
 @export var host_hbox:VBoxContainer
 
 @export var steam_name_label:Label
+@export var steam_refresh_lobbies_button:Button
+@export var steam_lobby_itemlist:ItemList
+
+var STEAM_APP_ID = 480
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,19 +21,53 @@ func _ready() -> void:
 	multiplayer.connected_to_server.connect(_on_connected_to_server)
 
 	#Too Many Sheep: 2410820
-
-	var initialize_response: Dictionary = Steam.steamInitEx( true, 480)
+	OS.set_environment("SteamAppId", str(STEAM_APP_ID))
+	OS.set_environment("SteamGameId", str(STEAM_APP_ID))
+	
+	var initialize_response: Dictionary = Steam.steamInitEx()
 	print("Did Steam initialize?: %s " % initialize_response)
+	
+	steam_name_label.text = Steam.getPersonaName()
+	Steam.lobby_match_list.connect(_on_lobby_match_list)
+	Steam.lobby_created.connect(_on_lobby_created)
+	Steam.lobby_data_update.connect(_on_lobby_data_update)
+
+	
+func _process(delta: float) -> void:
+	Steam.run_callbacks()
+
+func steam_create_lobby() -> void: 
+	print("Creating Lobby")
+	Steam.createLobby(Steam.LOBBY_TYPE_PUBLIC, 2)
+
+func steam_lobby_refresh() -> void:
+	print("Requestion Lobby Refresh")
+	Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_CLOSE)
+	Steam.requestLobbyList()	
+
+func _on_lobby_match_list(lobbies: Array):
+	steam_lobby_itemlist.clear()
+	print("Received ", lobbies.size(), " lobbies.")
+	for lobby_id in lobbies:
+		# Request data for each lobby (name, metadata, etc.)
+		Steam.requestLobbyData(lobby_id)
+
+func _on_lobby_data_update(success: bool, lobby_id: int, member_id: int):
+	if success:
+		var name = Steam.getLobbyData(lobby_id, "name")
+		if name.is_empty():
+			name = "Lobby " + str(lobby_id)
+		steam_lobby_itemlist.add_item(name + " (ID: " + str(lobby_id) + ")", null, false)
 
 
-	# steam_name_label.text = Steam.getPersonaName()
-	
-	# Steam.addRequestLobbyListDistanceFilter(Steam.LOBBY_DISTANCE_FILTER_CLOSE)
-	
-	#Steam.lobby_match_list.connect(
-	#	func(lobby_list:Array):
-	#		print(lobby_list)
-	#)
+func _on_lobby_created(connect: bool, lobby_id: int):
+	if connect:
+		print("Lobby created! Lobby ID: ", lobby_id)
+		# You can now set metadata or wait for others to join
+		Steam.setLobbyData(lobby_id, "name", "My Godot Lobby")
+	else:
+		print("Failed to create lobby. Error code: ")
+
 
 func _on_host_button_pressed() -> void:
 	not_connected_hbox.hide()
